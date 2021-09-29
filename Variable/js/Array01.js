62edcb11623f4ebf0efdedc320c283880ba1fd79
p000059 <marcelo.acad@gmail.com>
2021-09-28 22:36:46 -0300

    20210928223600

---
+++ Variable/js/Array01.js
@@ -0,0 +1,5 @@
+
+let num = [5,9,4] // cria uma variável do tipo array
+num[3] = 7 // adiciona na posição 3 o valor de 7 no array
+num.push(8) // adiciona na última posição o valor 8
+num.length //retorna o valor do tamanho do array 
