d2a90f855b0e8d32bb6bd8b98dc25b766d8a67ca
p000059 <marcelo.acad@gmail.com>
2021-09-29 16:12:18 -0300

    20210929151000

--- Variable/js/Array01.js
+++ Variable/js/Array01.js
@@ -1,5 +1,6 @@
 
 let num = [5,9,4] // cria uma variável do tipo array
-num[3] = 7 // adiciona na posição 3 o valor de 7 no array
-num.push(8) // adiciona na última posição o valor 8
-num.length //retorna o valor do tamanho do array 
+console.log("Adiciona na posição 3 o valor 7 = " + (num[3] = 7))  // adiciona na posição 3 o valor de 7 no array
+console.log("Adiciona na ultima posição = " + num.push(8)) // adiciona na última posição o valor 8
+console.log("Tamanho do Array = " + num.length) //retorna o valor do tamanho do array 
+console.log("Ultima posição do Array = "+num[num.length -1]) //retorna a ultima posição do array
