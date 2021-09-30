ac5b9e68062e875a96dfff02082ab42ac1c91574
p000059 <marcelo.acad@gmail.com>
2021-09-29 22:23:56 -0300

    20210929212300

---
+++ ForEach/ForEach02.js
@@ -0,0 +1,22 @@
+//Este programa utiliza um for melhorado, com uma expressão mais curta, mas utilizando uma função de soma.
+
+var numeros = [0,1,2,3,4,5,6,7,8,9,10]
+let total = 0;
+
+function soma(item){
+    total = total + item;
+    return total;
+}
+
+for(let i in numeros){
+    console.log(soma(numeros[i]));
+}
+
+
+
+
+
+
+
+
+
