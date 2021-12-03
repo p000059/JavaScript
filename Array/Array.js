e7a3ec043f1776843d9d2522f353ffba3f32118b
p000059 <marcelo.acad@gmail.com>
2021-12-03 00:37:57 -0300

    deleted:    Array/Array.js

--- Array/Array.js
+++
@@ -1,8 +0,0 @@
-//Arrays são tipos especiais de objetos. No exemplo abaixo, ambos podem podem ser verificados com typeof(), onde o retorno será um objeto.
-
-let person = {firstName:"John", lastName:"Doe", age:46};
-let person1 = ["John", "Doe", 46];
-
-console.log(person);
-
-
