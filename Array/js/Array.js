1ba58ee40455eaba949d55fcd7d4085fcde0f2fb
p000059 <marcelo.acad@gmail.com>
2022-01-20 20:20:12 -0300

    20220120082000PM

--- Array/js/Array.js
+++ Array/js/Array.js
@@ -1,13 +1,90 @@
-//A função abaixo vai atribuir a cada id do HTML uma operação em referência ao Array.
+let arrayObj = new Array();
 
-function showElements(){
-    
-    let corBandeira = ["Vermelha", "Amarela", "Preta", "Branca", "Quadriculada"];
+//Arrow Function
+// let somar = (a,b) => a+b;
+// let somar = function(a,b) => {return a+b;}
 
-    document.getElementById("array").innerHTML = "Array[0] = " + corBandeira[0]; //Mostra índice na posição 0
-    document.getElementById("size").innerHTML = "Array.length = " + corBandeira.length; //Mostra tamanho da bandeira
-    document.getElementById("lastPosition").innerHTML = "Array[length - 1] = " + corBandeira[corBandeira.length - 1]; //Mostra a última posição do Array
-    document.getElementById("total").innerHTML = "Array[" + corBandeira + "]"; //Mostra todo o contéudo do Array.
+// Mascara CNPJ
+// replace
+// /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/ 
+// "$1.$2.$3/$4-$5"
+
+// Mascara CPF
+// replace
+// /^(\d{3})(\d{3})(\d{3})(\d{2})/ => Mascara CPF
+// "$1.$2.$3-$4"
+
+function createObject() {
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let fone = document.getElementById('fone').value;
+
+    let objPerson = new Object()
+
+    objPerson.name = name;
+    objPerson.email = email;
+    objPerson.fone = mtel(fone);
+
+    arrayObj.push(objPerson);
+
+    console.log(objPerson);
+    console.log(objPerson.name, objPerson.email, objPerson.fone);
+
 }
 
+function mtel(v) {
 
+    if (v.length === 11) {
+
+        v = v.replace(/D/g, ""); //Remove tudo o que não é dígito
+        v = v.replace(/(\d{2})?(\d{5})?(\d{4})/, "($1)$2-$3");
+
+    } else if (v.length !== 10) {
+
+        v = v.replace(/D/g, ""); //Remove tudo o que não é dígito
+        v = v.replace(/(\d{2})?(\d{4})?(\d{4})/, "($1)$2-$3");
+    }
+
+    return v;
+}
+
+function openModal() {
+
+    let modal = document.getElementById('openModal');
+
+    if (typeof modal == 'undefined' || modal == null) {
+        return;
+    }
+    modal.style.display = 'block';
+
+}
+
+function closeModal() {
+    let modal = document.getElementById('openModal');
+
+    if (typeof modal == 'undefined' || modal == null) {
+        return;
+    }
+    modal.style.display = 'none';
+}
+
+function showArray() {
+
+    let table = document.getElementById('tableArray1');
+    let tbody = document.createElement('tbody');
+    let tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerHTML = arrayObj;
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
+}
+
+function clearFields() {
+
+    document.getElementById('name').value = "";
+    document.getElementById('email').value = "";
+    document.getElementById('fone').value = "";
+}
--- Functions/js/AnonymousFunction.js
+++ Functions/js/AnonymousFunction.js
@@ -1,49 +1,128 @@
-var power = function(base, exponent){
-    
+(function() {
+
+    $('carregando').ride();
+
+});
+
+let arrayObject = ["10", "20", "30", "40"]
+
+var power = function(base, exponent) {
+
     var result = 1;
-    
-    for(let count = 0; count < exponent; count++){
+
+    for (let count = 0; count < exponent; count++) {
         result = result * base;
     }
-    
+
     return result;
 }
 
 //Essa função anônima retorna um resultado para a variável makeNoise.
-var makeNoise = function(){
+var makeNoise = function() {
     var msg = "Pling!";
     return msg;
 }
 
 //Essa função anônima recebe um parâmetro e retorna um resultado de acordo com a condição estabelecida no if.
-var retornaResultado = function(n){
-	if(n % 2 === 0){
-		return "par";
-	}
-	else{
-		return "impar";
-	}
+var retornaResultado = function(n) {
+    if (n % 2 === 0) {
+        return "par";
+    } else {
+        return "impar";
+    }
 }
 
 //Essa função utiliza de parâmetros opcionais 
-var retornaResultado = function(x = 2, y = 3){
-    if(x !== y){
+var retornaResultado = function(x = 2, y = 3) {
+    if (x !== y) {
         return x + y;
-    }
-    else if(x === y){
+    } else if (x === y) {
         return x - y;
     }
 }
 
-function fatorial(n){
+function fatorial(n) {
     let fat = 1;
-    for(let c = n; c > 1; c--){
+    for (let c = n; c > 1; c--) {
         fat *= c;
     }
     return fat;
 }
 
-function countWhile(n){
+function showArray() {
 
-    
+    let table = document.getElementById('tableArray');
+    let tbody = document.createElement('tbody');
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+
+    td.innerHTML = arrayObject;
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+    table.appendChild(tbody);
 }
+
+function arrayMap() {
+
+    const stringToInt = (x) => parseInt(x);
+    const arrayResult = arrayObject.map(stringToInt);
+    document.getElementById('')
+}
+
+function bodyMap() {
+    document.getElementById('carregando').innerHTML = '<a href="pages/Map.html" >Map</a>';
+}
+
+const subtrair = (a, b) => a - b;
+const aplicacaoOperacao = (a, b, operacao) => operacao(a, b);
+
+let resultado = aplicacaoOperacao(4, 3, subtrair);
+
+console.log(resultado);
+
+const sumX = (x) => (y) => x + y;
+
+const sumTwo = sumX(2);
+
+console.log(sumTwo(5));
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+
+    let tab = document.querySelectorAll('.header-tab'), //class of your tab button
+        info = document.querySelector('.header'), //class of  your menu with tab buttons
+        tabContent = document.querySelectorAll('.tab-content'); //class of your tab with some content
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('header-tab')) { //class of your tab button
+            for (let i = 0; i < tab.length; i++) {
+                tab[i].classList.remove('show'); //if you want to delete some styles from unactive button
+                event.target.classList.add('show'); //if you want to add some styles on active button
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                }
+            }
+        }
+    });
+
+
+});
