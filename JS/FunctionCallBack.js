3aa48df61da67fb6096ef727f36e0ddf2db1f6a3
p000059 <marcelo.acad@gmail.com>
2023-01-04 10:39:06 -0300

    deleted:    FunctionCallBack.html 	deleted:    JS/FunctionCallBack.js 	modified:   JS/FunctionsCallBack.js 	deleted:    sass/Buttons.scss 	deleted:    sass/Inputs.scss 	deleted:    sass/Struct.scss 	deleted:    sass/Style.scss

--- JS/FunctionCallBack.js
+++
@@ -1,9 +0,0 @@
-function fn(cb){
-    console.log(typeof cb)
-}
-
-function callBack(){
-    console.log("Function CallBack")
-}
-
-fn(callBack)
--- JS/FunctionsCallBack.js
+++ JS/FunctionsCallBack.js
@@ -1,11 +1,32 @@
-function fn(callBack){
+let vector = new Array()
+
+function fn(callBack) {
     console.log(callBack)
-    const x = callBack
+    let x = callBack
     document.getElementById("ltext").innerHTML = x
 }
 
-function callBack(text){
+function callBack(text) {
     return text
 }
 
-fn(callBack("Call Back Function"))
+function callBackSum() {
+
+}
+
+function receiveNumber() {
+
+    let number = document.getElementById('number').value
+    vector.push(parseInt(number))
+
+    console.log(vector)
+}
+
+function sum() {
+    let result = 0
+    for (let i = 0; i < vector.length; i++) {
+        result += vector[i]
+    }
+
+    document.getElementById('ltotal').innerHTML = result
+}
