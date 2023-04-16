3670a7d9a9b4c760aa8e5118b54c5b05d4f860d8
p000059 <marcelo.acad@gmail.com>
2023-04-16 02:57:56 -0300

    update Numeric String

--- Numeric/js/Numeric.js
+++ Numeric/js/Numeric.js
@@ -1,54 +1,173 @@
-(function(){
-    let id_divBox = document.querySelector('#div-box')
-    
-    const largeNumberOne = 1_000
-    let divOne = document.createElement('div')
-    divOne.id = 'numberOne'
-    divOne.textContent = largeNumberOne
-    
-    const largeNumberTwo = 1_200
-    let divTwo = document.createElement('div')
-    divTwo.id = 'numberTwo'
-    divTwo.textContent = largeNumberTwo
-    
-    const numberThree = '46'
-    let divThree = document.createElement('div')
-    divThree.id = 'numberThree'
-    divThree.textContent = Number.parseInt(numberThree, 10)
-
-    const numberRound = 2.6
-    let divNumberRound = document.createElement('div')
-    divNumberRound.id = 'number-round'
-    divNumberRound.textContent = `Round 2.6 = ${Math.round(numberRound)}`
-
-    const numberFloor = 2.6
-    let divNumberFloor = document.createElement('div')
-    divNumberFloor.id = 'number-floor'
-    divNumberFloor.textContent = `Floor 2.6 = ${Math.floor(numberFloor)}`
-
-    const numberCeil = 2.6
-    let divNumberCeil = document.createElement('div')
-    divNumberCeil.id = 'number-ceil'
-    divNumberCeil.textContent = `Ceil 2.6 = ${Math.ceil(numberCeil)}`
-
-    id_divBox.appendChild(divOne)
-    id_divBox.appendChild(divTwo)
-    id_divBox.appendChild(divThree)
-    id_divBox.appendChild(divNumberRound)
-    id_divBox.appendChild(divNumberFloor)
-    id_divBox.appendChild(divNumberCeil)
+(function () {
+    let div = document.querySelector('#box')
+    div.appendChild(createDiv(`Large Number One: ${largeNumberOne(1_000_00)}`))
+    div.appendChild(createDiv(`Large Number Two: ${largeNumberTwo(1_200)}`))
+    div.appendChild(createDiv(`Number.parseInt('n'): ${parseIntNumber('46.4856')}`))
+    div.appendChild(createDiv(`Number.parseFloat('n'): ${parseFloatNumber('46.4856')}`))
+    div.appendChild(createDiv(`variable.toFixed(46.4856): ${roundedNumber(46.4856)}`))
+    div.appendChild(createDiv(`variable.toLocale(46.4856): ${roundedNumberLocale(46.4856)}`))
+    div.appendChild(createDiv(`Floor (46.4856): ${numberFloor(46.4856)}`))
+    div.appendChild(createDiv(`Ceil (46.4856): ${numberCeil(46.4856)}`))
+    div.appendChild(createDiv(`Prime Number: ${primeNumber(40)}`))
+    div.appendChild(createDiv(`Fibonnaci: ${fibonnaciNumber(15)}`))
+    div.appendChild(createDiv(`Number greater than variable? ${evaluateCondition(11)}`))
+    div.appendChild(createDiv(`Factorial Number: ${factorialNumber(5)}`))
+    div.appendChild(createDiv(`Bigger Number: ${biggerNumber(4, 5)}`))
+    div.appendChild(createDiv(`Search Number in Array: ${searchNumber(8)}`))
+    div.appendChild(createDiv(`Bigger number in Array: ${searchBiggerNumber()}`))
+    div.appendChild(createDiv(`Small number in Array: ${searchSmallNumber()}`))
+    div.appendChild(createDiv(`Small number in array.find(): ${searchSmallNumberFind()}`))
 })()
 
-function evaluateCondition(grade){
-    //wrong
-    /* if (grade >= 10) {
-        return true;
-    } else {
-        return false;
-    } */
-    
-    //Sure
-    return grade >= 10
+function createDiv(object) {
+    let div = document.createElement('div')
+    div.textContent = object
+    return div
 }
 
-console.log(evaluateCondition(10))
+function largeNumberOne(number) {
+    const largeNumber = number
+    return largeNumber
+}
+
+function largeNumberTwo(number) {
+    const largeNumber = number
+    return largeNumber
+}
+
+function parseIntNumber(number) {
+    let objNumber = Number.parseInt(number)
+    return objNumber
+}
+
+function parseFloatNumber(number) {
+    let objNumber = Number.parseFloat(number)
+    return objNumber
+}
+
+function roundedNumber(number) {
+    let numberRound = number.toFixed(2)
+    return numberRound
+}
+
+function roundedNumberLocale(number) {
+    let numberLocale = number.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    return numberLocale
+}
+
+function numberFloor(number) {
+    return Math.floor(number)
+}
+
+function numberCeil(number) {
+    return Math.ceil(number)
+}
+
+function primeNumber(n) {
+    let array = new Array()
+    for (let i = 0; i <= n; i++) {
+        if ((n > 0) && ((i === 2) || (i % 2 === 1))) {
+            array.push(i)
+        }
+    }
+    return array
+}
+
+function fibonnaciNumber(number) {
+    let previous = 0
+    let current = 1
+    let next = 1
+    let array = new Array()
+
+    for (let i = 0; i < number; i++) {
+        array.push(next)
+        previous = current + next
+        current = next
+        next = previous
+    }
+
+    return array
+}
+
+function factorialNumber(number) {
+    let resultNumber = number
+
+    for (let i = (number - 1); i > 0; i--) {
+        resultNumber = resultNumber * i
+    }
+    return resultNumber
+
+    /* if (number === 0 || number === 1) {
+        return 1
+    } else {
+        return number * factorialNumber(number - 1)
+    } */
+}
+
+function biggerNumber(numberOne, numberTwo) {
+    if (numberOne > numberTwo) {
+        return numberOne
+    }
+    return numberTwo
+}
+
+function searchNumber(number) {
+    let array = [15, 8, 4, 89, 47, 5, 12, 26]
+    let resultNumber = 0
+
+    array.forEach((x) => {
+        if (number === x) {
+            resultNumber = x
+        }
+    })
+
+    return resultNumber
+}
+
+function searchBiggerNumber() {
+    let array = [15, 113, 4, 89, 47, 175, 12, 26]
+    let resultNumber = 0
+
+    /* for(let i = 0; i < array.length; i++){
+        if(array[i] > resultNumber){
+            resultNumber = array[i]
+        } 
+    } */
+
+    array.forEach((x) => {
+        if (x > resultNumber) {
+            resultNumber = x
+        }
+    })
+    return resultNumber
+}
+
+function searchSmallNumber() {
+    let array = [15, 113, 4, 89, 47, 200, 3, 12, 26]
+    let resultNumber = 0
+
+    array.forEach((x) => {
+        if (x < array[x]) {
+            resultNumber = x
+        }
+    })
+
+    return resultNumber
+}
+
+function searchSmallNumberFind() {
+    let array = [15, 113, 3, 89, 47, 200, 2, 12, 26]
+    let resultNumber = 0
+
+    array.find((x) => {
+        if (x < array[x]) {
+            resultNumber = x
+        }
+    })
+
+    return resultNumber
+}
+
+function evaluateCondition(number) {
+    return number >= 10
+}
--- String/js/String.js
+++ String/js/String.js
@@ -1,65 +1,114 @@
-var maximum_experience = 'maximum experience'
-document.querySelector('#string-simple').innerHTML = maximum_experience
+function createLabel(object) {
+    let label = document.createElement('label')
+    label.className = 'labels'
+    label.textContent = object
+    return label
+}
 
-var edit_string = document.querySelector('#btn-edit-string')
-edit_string.addEventListener('click', function () {
-    let string_show = document.getElementById('#string-simple')
-    console.log(string_show)
-})
+function clearResult(object) {
+    document.querySelector(object).innerHTML = ''
+}
 
 var btnInterpolation = document.querySelector('#btnInterpolation')
-btnInterpolation.addEventListener('click', function () {
-    let stringInterpolation = 'Experience'
-    let stringInterpolationAdd = `Maximum ${stringInterpolation}`
-    document.querySelector('#string-interpolation').value = stringInterpolationAdd
-})
+btnInterpolation.addEventListener('click', () => stringInterpolation())
 
 var btnSubstring = document.querySelector('#btnSubstring')
-btnSubstring.addEventListener('click', function () {
-    let subString = 'MMMMarcelo Guimbaaaa'
-    let subStringResult = subString.substring(3, 17)
-    document.querySelector('#sub-string').value = subStringResult
-})
+btnSubstring.addEventListener('click', () => showSubtring())
+
+var btnInvertString = document.querySelector('#btn-invert-string')
+btnInvertString.addEventListener('click', () => invertString())
 
 var btnCapitalize = document.querySelector('#btnCapitalize')
-btnCapitalize.addEventListener('click', function () {
+btnCapitalize.addEventListener('click', () => capitalizeString())
+
+var btnFirstCharacter = document.querySelector('#btnFirstCharacter')
+btnFirstCharacter.addEventListener('click', () => firstCharacterString())
+
+var btnLastCharacter = document.querySelector('#btnLastCharacter')
+btnLastCharacter.addEventListener('click', () => lastCharacterString())
+
+var btnLowerCase = document.querySelector('#btn-lower-case')
+btnLowerCase.addEventListener('click', () => lowerCaseString())
+
+var btnUpperCase = document.querySelector('#btn-upper-case')
+btnUpperCase.addEventListener('click', () => upperCaseString())
+
+var btn_capitalize = document.querySelector('#btn-capitalize-one')
+btn_capitalize.addEventListener('click', () => stringCapitalize())
+
+var btnVowel = document.querySelector('#btn-vowel')
+btnVowel.addEventListener('click', () => vowelCount())
+
+function stringInterpolation() {
+    document.querySelector('#result-interpolation').innerHTML = ''
+    let stringInterpolation = document.querySelector('#string-interpolation').value
+    let stringInterpolationAdd = `Maximum ${stringInterpolation}`
+    let result = document.querySelector('#result-interpolation')
+    result.appendChild(createLabel(stringInterpolationAdd))
+}
+
+function showSubtring() {
+    let subString = 'MMMMarcelo Guimbaaaa'
+    let subStringResult = subString.substring(3, 17)
+    let result = document.querySelector('#sub-string')
+    result.appendChild(createLabel(subStringResult))
+}
+
+function capitalizeString() {
     let stringCapitalize = 'marcelo guimba'
     let firstName = stringCapitalize[0].toUpperCase() + stringCapitalize.substring(1, 7)
     let lastName = stringCapitalize[8].toUpperCase() + stringCapitalize.substring(9)
-    document.querySelector('#string-capitalize').value = `${firstName} ${lastName}`
-})
+    let stringResult = `${firstName} ${lastName}`
+    let result = document.querySelector('#string-capitalize')
+    result.appendChild(createLabel(stringResult))
+}
 
-var btnFirstCharacter = document.querySelector('#btnFirstCharacter')
-btnFirstCharacter.addEventListener('click', function () {
+function firstCharacterString() {
     let maximumCharacter = 'maximum character'
     let firstCaracter = maximumCharacter.substring(0, 1)
-    document.querySelector('#first-caracter').value = firstCaracter
-})
+    let result = document.querySelector('#first-character')
+    result.appendChild(createLabel(firstCaracter))
+}
 
-var btnLastCharacter = document.querySelector('#btnLastCharacter')
-btnLastCharacter.addEventListener('click', function () {
+function lastCharacterString() {
     let stringCharacter = 'Maximum Experience'
     let lastCharacter = stringCharacter[stringCharacter.length - 1]
-    document.querySelector('#last-character').value = lastCharacter
-})
+    let result = document.querySelector('#div-last-character')
+    result.appendChild(createLabel(lastCharacter))
+}
 
-var btnLowerCase = document.querySelector('#btnLowerCase')
-btnLowerCase.addEventListener('click', function () {
-    let stringLowerCase = 'MAXIMUM EXPERIENCE'
-    let lowerCase = stringLowerCase.toLocaleLowerCase()
-    document.querySelector('#lower-case').value = lowerCase
-})
+function lowerCaseString() {
+    document.querySelector('#lower-case').innerHTML = ''
+    let stringLowerCase = document.querySelector('#lower-case').value
+    let upperCase = stringLowerCase.toLowerCase()
+    let result = document.querySelector('#string-lower-case')
+    result.appendChild(createLabel(upperCase))
+}
 
-var btnUpperCase = document.querySelector('#btnUpperCase')
-btnUpperCase.addEventListener('click', function () {
-    let stringUpperCase = 'maximum experience'
+function upperCaseString() {
+    document.querySelector('#upper-case').innerHTML = ''
+    let stringUpperCase = document.querySelector('#upper-case').value
     let upperCase = stringUpperCase.toUpperCase()
-    document.querySelector('#upper-case').value = upperCase
-})
+    let result = document.querySelector('#string-upper-case')
+    result.appendChild(createLabel(upperCase))
+}
 
-var btn_capitalize = document.querySelector('#btn-capitalize')
-btn_capitalize.addEventListener('click', function () {
-    let string_captalize = 'maximum'
-    let stringResult = string_captalize[0].toUpperCase() + string_captalize.substring(1)
-    document.querySelector('#stringcapitalize').value = stringResult
-})
+function stringCapitalize() {
+    let string_captalize = document.querySelector('#string-capitalize-one').value
+    let stringResult = string_captalize[0].toUpperCase() //+ string_captalize.substring(1)
+    let result = document.querySelector('#string-capitalize-one')
+    result.appendChild(createLabel(stringResult))
+}
+
+function invertString() {
+    document.querySelector('#result-invert').innerHTML = ''
+    let stringName = document.querySelector('#invert-string').value
+    let resultString = stringName.split('').reverse().join('')
+    let result = document.querySelector('#result-invert')
+    result.appendChild(createLabel(resultString))
+}
+
+function vowelCount(){
+    document.querySelector('#result-vowel').innerHTML = ''
+    let 
+}
